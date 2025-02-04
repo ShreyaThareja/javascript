9e85045c43ba5ca055679bc5a54b108f0b115a57
ShreyaThareja <119164680+ShreyaThareja@users.noreply.github.com>
2025-02-04 05:47:24 +0000

    conversion of data types

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,34 @@
+let score= "33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+//right now 33 is a string, but we want to convert it into a number
+let valueInNumber= Number(score)      
+console.log(typeof valueInNumber)
+
+/*
+"33" => 33
+"33abc" => NaN
+"true" => 1
+"false" => 0
+*/
+
+
+//converting a number to boolean
+let isLoggedIn= 1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+//converting number to string
+let someNumber= 33
+let stringNumber= String(someNumber)
+console.log(stringNumber) 
+//this gives us the output as 33. so now we'll confirm using typeof ki ye
+// jo 33 hmare paas output mein aaya hai vo string hai ya number
+
+console.log(typeof stringNumber)
+//output: string, which confirms that the number has been converted into string
+
+
