395bba27d5d6ea219766b651a240de00d11e05ad
ShreyaThareja <119164680+ShreyaThareja@users.noreply.github.com>
2025-02-18 14:17:25 +0000

    more code

--- functions/iife.js
+++ functions/iife.js
@@ -1,1 +1,10 @@
 //Immediately Invoked Function Expression
+/*IIFE (Immediately Invoked Function Expression) is used in JavaScript primarily 
+to create a private scope and avoid polluting the global namespace.
+It is a function that is defined and executed immediately.*/
+
+
+
+(function chai(){
+    console.log(`db connected`)
+})();          //semicolon is written here
