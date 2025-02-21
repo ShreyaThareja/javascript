719e90e7b46577dc1c0be14a88d972d62871224c
ShreyaThareja <119164680+ShreyaThareja@users.noreply.github.com>
2025-02-21 09:22:45 +0000

    more code

--- controlflow/newfile.js
+++ controlflow/newfile.js
@@ -5,3 +5,10 @@ const newnums= mynums.filter(  (num)  =>  num > 4)
 console.log(newnums)
 
 console.log(newnums)
+
+
+
+//map operation
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+const newNums= myNums.map(   (num) =>  num + 10)
+console.log(newNums)
---
+++ controlflow/newfile2.js
@@ -0,0 +1,5 @@
+//map operation
+
+const mynums=[1,2,3,4,5,6,7,8,9,10]
+
+mynums.map(   ())
