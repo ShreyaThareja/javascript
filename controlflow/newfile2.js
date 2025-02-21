dda5bf92f046995cf4d28394579689e8f75c7828
ShreyaThareja <119164680+ShreyaThareja@users.noreply.github.com>
2025-02-21 09:32:38 +0000

    more code

--- controlflow/newfile2.js
+++ controlflow/newfile2.js
@@ -1,5 +1,6 @@
-//map operation
+//chaining of map and filter
 
 const mynums=[1,2,3,4,5,6,7,8,9,10]
+const newnums= mynums.map(  (num) => num * 10).map(   (num) => num + 1).filter(   (num) => num>= 40)
 
-mynums.map(   ())
+console.log(newnums)
